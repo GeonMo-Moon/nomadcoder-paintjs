98b920aed2788266a6d76392bbde768f15346196
moon geon mo <rlaclch@gmail.com>
2022-05-22 17:46:42 +0900

    main

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "black";
 const CANVAS_SIZE = 700;
@@ -10,7 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -54,12 +56,18 @@ function handleCanavsClick(){
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanavsClick);
+    canvas.addEventListener("contextmenu", handleCM);
+
 }
 
 Array.from(colors).forEach(color => 
@@ -87,4 +95,16 @@ function handleModeClick(){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "hello";
+    link.click();
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
